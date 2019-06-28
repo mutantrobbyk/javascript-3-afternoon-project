fe0c6850d75e6f7da1b62ff5b019da46e5aa4f70
Robert Knowles <robertknowles@Roberts-MacBook-Pro.local>
2019-06-28 16:42:16 -0600

    for-in

--- for-in-delete.js
+++ for-in-delete.js
@@ -11,25 +11,25 @@
   In the example below, we are accessing the property values. Uncomment the code below, run it and look at what prints in the console.
 */
 
-// var values = {
-//   one: 'These',
-//   two: ' are',
-//   three: ' the',
-//   four: ' property',
-//   five: ' values.'
-// } 
+var values = {
+  one: 'These',
+  two: ' are',
+  three: ' the',
+  four: ' property',
+  five: ' values.'
+} 
 
-// for(var key in values) {
-//   console.log(values[key])
-// }
+for(var key in values) {
+  // console.log(values[key])
+}
 
 /*
   In this next example, we are accessing the property names themselves. Uncomment the code below, run it and look at what prints in the console.
 */
 
-// for(var key in values) {
-//   console.log(key)
-// }
+for(var key in values) {
+  // console.log(key)
+}
 
 
 
@@ -40,6 +40,11 @@
 */
 
 function showValues( obj ) {
+  let str = ''
+  for(var key in obj){
+  str += obj[key]
+}
+return str
   //Code Here
 }
 
@@ -52,8 +57,14 @@ function showValues( obj ) {
   Write a for in loop that loops over the object and changes any value that is greater than 10 to 0. 
   Return the updated object.
 */
-
-//Code Here
+function greaterThan10 (obj) {
+  for(let key in obj) {
+    if(obj[key] > 10){
+    obj[key] = 0
+    }
+  }return obj
+  }
+  //Code Here
 
 
 
@@ -64,7 +75,11 @@ function showValues( obj ) {
   Write a for in loop that loops over the object and changes every value to be itself multipled by 2.
   Return the updated object.
 */
-
+function double (obj) {
+  for (let key in obj) {
+    obj[key] *= 2
+  }return obj
+}
 //Code Here
 
 
@@ -78,7 +93,14 @@ function showValues( obj ) {
   If the property name starts with an 'sh', concatenate the value to the string variable.
   By the end of the for in loop, you should have a sentence, return that sentence.
 */
-
+function secrets (obj) {
+  let str = ''
+  for (let key in obj) {
+    if (key.startsWith ('sh')) {
+    str += obj[key]
+  }
+}return str
+}
 //Code Here
 
 
@@ -89,15 +111,15 @@ function showValues( obj ) {
   Uncomment the example below to see a for in loop deleting all the properties inside an object.
 */
 
-// var deleteAllThethings = {
-//   one: 1,
-//   two: 2,
-//   three: 3
-// }
+var deleteAllThethings = {
+  one: 1,
+  two: 2,
+  three: 3
+}
 
-// for(var key in deleteAllThethings) {
-//   delete deleteAllThethings[key]
-// }
+for(var key in deleteAllThethings) {
+  delete deleteAllThethings[key]
+}
 
 // console.log(deleteAllThethings)
 
@@ -109,6 +131,10 @@ function showValues( obj ) {
   Write a function called removePassword that takes in an object.
   Delete the property password and return the object.
 */
+function removePassword (obj) {
+  delete obj.password
+  return obj
+}
 
 //Code Here
 
@@ -128,7 +154,10 @@ var deleteTheBigNumbers = {
 /*
   Write a for in loop that deletes every property from the object deleteTheBigNumbers whose value is greater than 100.
 */
-
+for (let key in deleteTheBigNumbers) {
+  if (deleteTheBigNumbers[key] > 100)
+  delete deleteTheBigNumbers[key]
+}
 //Code Here
 
 
@@ -141,7 +170,13 @@ var deleteTheBigNumbers = {
   If any property name starts with k, delete that property.
   Return the updated object.
 */
-
+function startsWithK (obj) {
+  for (let key in obj) {
+    if (key.startsWith ('k')) {
+      delete obj[key]
+    }
+  }return obj
+}
 //Code Here
 
 
@@ -156,7 +191,15 @@ var deleteTheBigNumbers = {
 
   (hint: the method includes() may be of use...)
 */
-
+function hiddenTreasure (obj) {
+  for (let key in obj) {
+    console.log(key)
+    if (!obj[key].includes('treasure')) {
+      console.log(key)
+      delete obj[key]
+    }
+  }return obj
+  }
 //Code Here
 
 
